9a15f8144ab833cc1b7ae2dc668d72d785bbf091
bidarMR97 <bidar.mr97@gmail.com>
2025-09-21 09:22:38 -0700

    Fix apostrophe escaping for Vercel build

--- src/components/EnDocs/BackGround/page.tsx
+++ src/components/EnDocs/BackGround/page.tsx
@@ -10,11 +10,14 @@ export default function BackGround() {
                 </h1>
             </div>
             <div className="w-[700px] h-[100px] dark:text-[white]">
-                <p> I'm currently an Engineer at Upstatement building things for the web with some awesome people. I recently graduated from Northeastern University after completing three awesome six-month co-ops at MullenLowe U.S., Starry, and Apple Music.
-
+                <p>
+                    I&apos;m currently an Engineer at Upstatement building things for the web with some awesome people. I recently graduated from Northeastern University after completing three awesome six-month co-ops at MullenLowe U.S., Starry, and Apple Music.
+                    <br /><br />
                     As a software engineer, I enjoy bridging the gap between engineering and design — combining my technical knowledge with my keen eye for design to create a beautiful product. My goal is to always build applications that are scalable and efficient under the hood while providing engaging, pixel-perfect user experiences.
+                    <br /><br />
+                    When I&apos;m not in front of a computer screen, I&apos;m probably snowboarding, cruising around on my penny board, or crossing off another item on my bucket list.
+                </p>
 
-                    When I'm not in front of a computer screen, I'm probably snowboarding, cruising around on my penny board, or crossing off another item on my bucket list.</p>
             </div>
         </div>
     )
