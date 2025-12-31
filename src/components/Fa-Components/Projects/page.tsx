65fdd6ef40525f1fbe4772af14c02ac8a3226d4d
bidarMR97 <bidar.mr97@gmail.com>
2025-12-31 05:26:26 -0800

    links updated

--- src/components/Fa-Components/Projects/page.tsx
+++ src/components/Fa-Components/Projects/page.tsx
@@ -8,10 +8,10 @@ export default function Projects() {
 
             <section className="lg:flex lg:absolute  lg:bg-[silver]/30 lg:h-[500px] lg:w-[770px]  lg:rounded-2xl ">
 
-                <h2 className="text-[18px] mt-[10px] text-center font-[fa11] font-bold sm:text-[25px] lg:text-[19px] lg:p-[5px] lg:absolute lg:mt-[50px] lg:pr-[50px]"> راهنمای آموزشی بازی <strong>Warcraft (دستیار دوتا)</strong></h2>
+                <h2 className="text-[18px] mt-[10px] text-center font-[fa11] font-bold sm:text-[25px] lg:text-[19px] lg:p-[5px] lg:absolute lg:mt-[50px] lg:pr-[50px]"> راهنمای آموزشی بازی <strong className="text-blue-600 hover:underline hover:text-blue-800"><a href="https://dota1.ir">Warcraft (دستیار دوتا)</a></strong></h2>
                 <p  className="text-center mt-[15px] leading-[35px] sm:text-[22px] sm:leading-[50px] lg:w-[600px] lg:leading-[50px] lg:text-[22px] lg:text-center lg:mt-[12%]">
                     در حال حاضر روی برنامه‌ی دستیار دوتا ۱ که
-                    راهنما برای بازی کامپیوتری <strong className="font-[michroma] text-blue-600 hover:underline hover:text-blue-800"><a href="https://dota1.ir/">Warcraft Dota 1</a></strong>
+                    راهنما برای بازی کامپیوتری <strong className="font-[michroma] text-blue-600 hover:underline hover:text-blue-800"><a href="https://dota1.ir">Warcraft Dota 1</a></strong>
                     هست کار می‌کنم. <br /> اگر پیشنهادی برای همکاری داشتین،از طریق{" "}
                     <a href="#contactme" className="font-bold text-blue-600 hover:underline hover:text-blue-800 transition-colors">
                         <br />  لینک‌های تماس<br />
